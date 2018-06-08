8aadaf5a7a1e420eb454bd96eee81afa1e906be4
Moroen <moroen@gmail.com>
2018-06-08 19:39:37 +0200

    Updated Dockerfiles to latest version of pytradfri

--- start.sh
+++ start.sh
@@ -1,15 +1,25 @@
 #/usr/bin/env bash
 
+set -e
+
+MACHINE=$(uname)
 PWD=$(pwd)
 
-#docker run \
-#  --net=host \
-#  -v /etc/localtime:/etc/localtime:ro \
-#  -v `pwd`:/usr/src/app \
-#  -t -i --rm ikea-plugin
 
-docker run \
-  -v /etc/localtime:/etc/localtime:ro \
-  -v `pwd`:/usr/src/app \
-  -p 1234:1234 \
-  -t -i --rm ikea-plugin python3 tradfri.tac
+  
+
+  case "$MACHINE" in
+  Linux) 
+    docker run \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v `pwd`:/usr/src/app \
+    -p 1234:1234 \
+    -t -i --rm ikea-plugin python3 tradfri.tac
+    ;;
+  Darwin)
+    docker run \
+    -v `pwd`:/usr/src/app \
+    -p 1234:1234 \
+    -t -i --rm ikea-plugin python3 tradfri.tac
+    ;;
+esac
