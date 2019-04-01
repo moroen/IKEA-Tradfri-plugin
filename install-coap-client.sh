806e4e5a6f723303ce0eaab31c51aac37541aed2
moroen <moroen@gmail.com>
2019-04-02 00:44:55 +0200

    Cleanup

--- install-coap-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
-cd libcoap
-./autogen.sh
-./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
-make
-sudo make install
