192c24cf5df71eb21d9bb78d4e230d27e560206f
Moroen <moroen@gmail.com>
2019-07-06 01:42:46 +0200

    Changed to tradfri master branch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='moroen@gmail.com',
     description='Controlling IKEA-Tradfri from Domoticz',
     packages=[],
-    dependency_links=['http://github.com/moroen/ikea-tradfri/tarball/development#egg=ikeatradfri-0.0.1'],
+    dependency_links=['http://github.com/moroen/ikea-tradfri/tarball/master#egg=ikeatradfri-0.0.1'],
     include_package_data=True,
     setup_requires=['Cython'],
     install_requires=['ikeatradfri==0.0.1'],
