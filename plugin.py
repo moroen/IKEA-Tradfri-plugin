b17e467de7966a8853c0dda03550fd7e1b047446
moroen <moroen@gmail.com>
2020-01-27 12:08:45 +0100

    Bump version: 0.8.0 → 0.8.1

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
 # Author: Moroen
 #
 """
-<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.8.0" author="moroen" version="0.8.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://www.google.com/">
+<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.8.1" author="moroen" version="0.8.1" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://www.google.com/">
     <description>
         <h2>IKEA Tradfri</h2><br/>
     </description>
