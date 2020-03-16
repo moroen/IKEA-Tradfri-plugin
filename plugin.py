28f0dc1014224ae1820ac6b28b473c40fa70fb62
moroen <moroen@gmail.com>
2020-03-16 17:48:28 +0100

    Bump version: 0.9.1 → 0.9.2

--- plugin.py
+++ plugin.py
@@ -8,7 +8,7 @@
 #
 
 """
-<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.9.1" author="moroen" version="0.9.1" externallink="https://github.com/moroen/IKEA-Tradfri-plugin">
+<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.9.2" author="moroen" version="0.9.2" externallink="https://github.com/moroen/IKEA-Tradfri-plugin">
     <description>
         <h2>IKEA Tradfri</h2><br/>
     </description>
@@ -55,7 +55,7 @@ site.main()
 
 _globalError = None
 
-_version = "0.9.1"
+_version = "0.9.2"
 
 # Need to set config before import from module
 try:
