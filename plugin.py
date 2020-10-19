b4e264b30aff079d3ac8f7c63a00ad18250de536
Moroen <moroen@gmail.com>
2020-10-20 00:54:54 +0200

    Bump version: 0.9.14 → 0.9.15

--- plugin.py
+++ plugin.py
@@ -8,7 +8,7 @@
 #
 
 """
-<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.9.14" author="moroen" version="0.9.14" externallink="https://github.com/moroen/IKEA-Tradfri-plugin">
+<plugin key="IKEA-Tradfri" name="IKEA Tradfri Plugin - version 0.9.15" author="moroen" version="0.9.15" externallink="https://github.com/moroen/IKEA-Tradfri-plugin">
     <description>
         <h2>IKEA Tradfri</h2><br/>
     </description>
@@ -59,7 +59,7 @@ site.main()
 
 _globalError = None
 
-_version = "0.9.14"
+_version = "0.9.15"
 
 _use_local_tradfricoap = False
 
